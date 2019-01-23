6ba09dff60299cb2f6923c6f718f8d2c2acc8010
plainolneesh <tanisha.faulkner@me.com>
2019-01-22 21:45:13 -0500

    Hello World Example

---
+++ IdeaProjects/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/** Hello world example for COSC 251 */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
